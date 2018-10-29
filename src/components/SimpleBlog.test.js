6a163ee78df685ddfa052a51981181f6fc8c9191
Sami Mäläskä <epsilon@welho.com>
2018-10-30 01:59:28 +0200

    Teht 13

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -10,6 +10,8 @@ describe.only('<SimpleBlog />', () => {
             likes: 10
         }
 
+        
+
         const blogComponent = shallow(<SimpleBlog blog={simpleBlog} />)
         console.log(blogComponent.debug())
 
@@ -22,5 +24,20 @@ describe.only('<SimpleBlog />', () => {
 
         console.log(contentDiv1.debug())
         console.log(contentDiv2.debug())
+    }),
+
+    it('test onClick', () => {
+        const simpleBlog = {
+            title: 'Testititle',
+            author: 'Testiauthor',
+            likes: 10
+        }
+
+        const mockHandler = jest.fn()
+        const blogComponent = shallow(<SimpleBlog blog={simpleBlog} onClick={ mockHandler }/>)
+        const button = blogComponent.find('button')
+        button.simulate('click')
+        button.simulate('click')
+        expect(mockHandler.mock.calls.length).toBe(2)
     })
 })
